4bb39a141b424a2fdd12d9de2550d7aa015aa6db
Wade Moore <wpmoore2@ncsu.edu>
2018-01-15 11:39:02 -0500

    Functional - Tested!

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Sync
 Syncs files from one location to another.
+
+Right now the tool is geared towards syncing to a OneDrive folder (effectively syncing
+to the cloud).  However, the OneDrive_base can be altered to point to any other directory.  
+
+
+The *sync_config.yml* may be used as a sample to understand the format for syncing directories.
+Avoid typing redundant paths by nesting those in common.
--- Sync_to_OneDrive.py
+++ Sync_to_OneDrive.py
@@ -2,17 +2,22 @@ import subprocess as sp
 import yaml
 import sys
 
-SYNC_CMD = "robocopy"
+# /S  - copy subdirectories, if not empty
+# /xo - only copy newer files (included those edited)
+SYNC_CMD = "robocopy /S /xo"
 CONFIG_PATH = 'sync_config.yml'
 
 #
 # Can specify to only print sync commands that would be executed
 PRINT_ONLY = False
-if len(sys.argv) > 1 and sys.argv[1] == "-n":
-    PRINT_ONLY == True
+if "-n" in sys.argv:
+    print ("Print only")
+    PRINT_ONLY = True
+
 QUIET = False
-if len(sys.argv) > 1 and sys.argv[1] == "-q":
-    QUIET == True
+if "-q" in sys.argv:
+    print ("Quiet mode")
+    QUIET = True
 
 
 def sync(src, dest, base_path, mirror):
@@ -43,15 +48,17 @@ try:
     config = yaml.load(open(CONFIG_PATH, 'r'))
 except Exception as err:
     print ("ERROR: Failed to parse {}.\n{}".format(CONFIG_PATH, str(err)))
+    raise err
 
 # Can define OndeDrive path in config, or assume in default user directory
 if config.get('OneDrive_base'):
     base_path = config['OneDrive_base']
 else:
     try:
-        user = sp.check_output("echo %USERNAME%").decode('UTF-8').trim()
+        user = sp.check_output("echo %USERNAME%", shell=True).decode('UTF-8').strip()
     except Exception as err2:
         print ("ERROR: Failed to get current user.\n{}".format(str(err2)))
+        raise err2
     base_path = "C:\\Users\\{}\\OneDrive".format(user)
 
 if config.get("NO_MIRROR") and config['NO_MIRROR'] is not None:
